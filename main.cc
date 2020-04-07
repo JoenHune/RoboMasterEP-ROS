ec16d1f4effd0b2f14bf05c127bb64c1a4c04dce
Joen <214629395@qq.com>
2020-04-07 23:08:02 +0800

    可读性增强

--- main.cc
+++ main.cc
@@ -9,55 +9,61 @@
 #include <string>
 
 in_addr_t get_robot_ip(int port) {
-    /* socket_fd --- socket文件描述符 创建udp套接字*/  
+    in_addr_t robot_ip = INADDR_NONE;
+
+    // 创建UDP套接字文件描述符
     int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if(socket_fd < 0)
         std::cerr << "socket";
 
-    /* 将套接字和IP、端口绑定 */  
+    // 设置UDP套接字的目标地址
     struct sockaddr_in target_addr;
-    int length = sizeof(target_addr);
-    //每个字节都用0填充
-    memset(&target_addr, 0, length);
+    memset(&target_addr, 0, sizeof(target_addr));
 
-    // 使用IPV4地址
-    target_addr.sin_family = AF_INET; 
-    // 端口
-    target_addr.sin_port = htons(port);
-    // 自动获取IP地址
-    target_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    target_addr.sin_family = AF_INET;                // 使用IPV4地址
+    target_addr.sin_port = htons(port);              // 端口
+    target_addr.sin_addr.s_addr = htonl(INADDR_ANY); // 任意IP地址
 
-    /* 绑定socket */
-    if(bind(socket_fd, (struct sockaddr *)&target_addr, length) < 0)
-        std::cerr << "bind error:";
+    // 绑定套接字和目标IP端口设置
+    if(bind(socket_fd, (struct sockaddr *)&target_addr, sizeof(target_addr)) < 0)
+        std::cerr << "bind error";
 
+    // 接收缓冲区
     char buffer[1024];
+    // 信息来源地址
     struct sockaddr_in source_addr;
-    in_addr_t robot_ip;
+    int length = sizeof(sockaddr_in);
 
     std::stringstream stream;
     std::string str, _template = "robot ip ";
 
     std::cout << "Waiting message from robot..." << std::endl;
     while(1) {
+        // 接收到信息
         if (0 < recvfrom(socket_fd, buffer, sizeof(buffer), 0, (struct sockaddr *)&source_addr, (socklen_t *)&length)) {
             stream << buffer;
             str = stream.str();
+
+            // 如果符合机器人广播IP信息的格式
             if (0 == str.compare(0, _template.length(), _template)) {
                 std::cout << "Got robot's ip: " << str.substr(_template.length()) << std::endl;
                 robot_ip = source_addr.sin_addr.s_addr;
-                return robot_ip;
+                break;
             }
         }
     }
 
     close(socket_fd);
 
-    return INADDR_NONE;
+    return robot_ip;
 }
 
 int main() {
     in_addr_t robot_ip = get_robot_ip(40926);
+
+    if (robot_ip != INADDR_NONE) {
+
+    }
 
     return 0;
 }
